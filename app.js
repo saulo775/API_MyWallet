99df5370cb4d63103a4d269c5bc5912575e63ffc
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-05-03 14:50:54 -0300

    Feat - Definindo Endpoints

--- app.js
+++ app.js
@@ -9,6 +9,41 @@ app.use(express.json());
 app.use(cors());
 
 
+app.post("/register", async (req, res)=>{
+    console.log("Route Create users");
+});
+
+app.get("/users", async (req, res)=>{
+    console.log("Route Consult users");
+});
+
+app.post("/login", async (req, res)=>{
+    console.log("Login");
+});
+
+app.get("/", async (req, res)=>{
+    console.log("Home");
+});
+
+app.post("/new_operation", async (req, res)=>{
+    console.log("Operacao");
+});
+
+app.post("/logout", async (req, res)=>{
+    console.log("Logout");
+});
+
+app.delete("/delete_operation", async (req, res)=>{
+    console.log("DELETAR Operacao");
+});
+
+app.put("/new_operation", async (req, res)=>{
+    console.log("ATUALIZAR Operacao");
+});
+
+
+
+
 app.listen(SERVIDOR_PORT, ()=>{
     console.log(chalk.blue.bold(`Servidor rodando na porta ${SERVIDOR_PORT}`));
 })
