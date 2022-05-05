48b10d0fede45a9b6ae7bd6f53421684087a0b7a
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-05-05 08:50:15 -0300

    FEAT - Cadastro de usuários no banco, email e usuário não podem ser iguais.

--- app.js
+++ app.js
@@ -2,31 +2,62 @@ import express from 'express';
 import cors from 'cors';
 import chalk from 'chalk';
 import { MongoClient } from 'mongodb';
+import "dotenv/config";
+import joi from 'joi';
+
 
 const app = express();
-const SERVIDOR_PORT = 5500;
+const SERVIDOR_PORT = process.env.PORT || 5500;
 
 app.use(express.json());
 app.use(cors());
 
 let database = null;
-const mongoClient = new MongoClient("mongodb://localhost/27017");
+const mongoClient = new MongoClient(process.env.DB_HOST);
 const promise = mongoClient.connect();
 promise.then(() => {
-    database = mongoClient.db("DB_MyWallet");
+    database = mongoClient.db(process.env.DB_NAME);
     console.log(chalk.bold.yellow("Conectado ao banco!!!"));
 });
 promise.catch((e) => {
     console.log("Problema ao conectar ao banco de dados", e);
 });
 
+app.post("/sign_up", async (req, res)=>{
+    const body = req.body;
+    const userSquema = joi.object({
+        username: joi.string().alphanum().min(3).max(30).required(),
+        email: joi.string().email().required(),
+        password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+        password_confirmation: joi.ref('password')
+    });
 
-app.post("/register", async (req, res)=>{
+    const userValidated = userSquema.validate(body, {abortEarly: false});
+
+    if (userValidated.error) {
+        return res.sendStatus(422);
+    }
+
+    const user = {...userValidated.value}
     
+    try{
+        const oldUser = await database.collection('users').findOne(
+            {$or: [{username: user.username},{email: user.email}]}
+        );
+        if (oldUser) {
+            return res.sendStatus(409);
+        }
+        await database.collection('users').insertOne({...user});
+        res.sendStatus(201);
+    }catch(e){
+        console.log("Erro ao cadastrar", e);
+        res.sendStatus(500);
+    }
 });
 
 app.get("/users", async (req, res)=>{
-    console.log("Route Consult users");
+    const users = database.collection("users").find().toArray();
+    console.log(users);
 });
 
 app.post("/login", async (req, res)=>{
